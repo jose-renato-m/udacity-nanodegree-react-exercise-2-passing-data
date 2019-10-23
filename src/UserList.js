819f2580932f57efda39449a0384ce0316cfd92b
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-10-23 15:38:07 +0000

    Display movies haven't been liked

--- src/UserList.js
+++ src/UserList.js
@@ -4,6 +4,8 @@ class UserList extends Component {
   render() {
    const {users, usersWhoLikedMovie} = this.props;
     
-   
+   if (!usersWhoLikedMovie || usersWhoLikedMovie.lenght === 0) {
+     return <p>None of the current users liked this movie.</p>;
+   }
   }
 }
