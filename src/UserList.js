c44804c2fb578ab45420d635f314d309a69fb3e6
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-10-23 17:55:32 +0000

    feat: Map users object

--- src/UserList.js
+++ src/UserList.js
@@ -7,5 +7,13 @@ class UserList extends Component {
    if (!usersWhoLikedMovie || usersWhoLikedMovie.lenght === 0) {
      return <p>None of the current users liked this movie.</p>;
    }
+    
+   const listofItems = usersWhoLikedMovie.map(id => (
+     <li key={id}>
+       <p>{users[id].name}</p>
+     </li>
+   ));
+
+   return <ul>{listofItems}</ul};
   }
 }
