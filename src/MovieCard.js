c3e240aebd747d7a56b55920cde2b08ea5e7e248
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-10-23 18:25:30 +0000

    Destructure object

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -2,7 +2,9 @@ import React, {Component} from "react";
 import UserList, from "./UserList";
 
 class MovieCard extends Component {
-
+  render() {
+   const {users, usersWhoLikedMovie, movieInfo} = this.props;
+  }
 }
 
 export default MovieCard;
