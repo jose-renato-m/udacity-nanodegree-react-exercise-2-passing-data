c221ad575da4395274e1468660edf074be800ec3
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-10-23 19:18:12 +0000

    feat: Destructure object

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,7 +2,11 @@ import React, {Component} from "react";
 import MovieCard from "./MovieCard";
 
 class Dashboard extends Component {
-
+  render() {
+    const {usersByMovie, users, movies} = this.props;
+    
+    
+  }
 }
 
 export default Dashboard;
