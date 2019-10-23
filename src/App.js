374a7ba9f4a6d76826c786ce1a0b49128354d85a
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-10-23 21:53:58 +0000

    feat: Map users by movie they liked

--- src/App.js
+++ src/App.js
@@ -102,6 +102,16 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.usersByMovie = {};
+    
+    profiles.forEach(profile => {
+      const movieID = profile.favoriteMovieID;
+      
+      if (this.usersByMovie[movieID]) {
+        this.usersByMovie[movieID].push(profile.userID);
+      } else {
+        this.usersByMovie[movieID] = [profile.userID];
+      }
+    });
   }
   
   render() {
